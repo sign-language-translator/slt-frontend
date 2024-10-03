59a3a464a4e9cffe05f35d2a6130be87af8c3bd8
Mudassar Iqbal <mdsriqb@gmail.com>
2024-10-03 19:21:58 +0500

    control avatar pose with world positions

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,116 +1,150 @@
+// @ts-check
+
+import React from "react";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-// import { Environment, Stats } from "@react-three/drei";
+// import { Environment, Stats, useFBX } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
 import "./index.css";
-import { isJsonString } from "@utils/index";
-// import PropTypes from 'prop-types';
+import { isJsonString } from "../../utils";
 
-// function AvatarGLTF({ url, texture, animations, basePosition, idleAnimation, transformFuctions, scale }) {
-//   return <primitive />;
-// }
+/**
+ * rotates the bone to look along the provided direction in world space.
+ * direction should be target minus bone's world position.
+ * @param {THREE.Object3D} bone - the bone to rotate
+ * @param {THREE.Vector3} direction - the world direction to look along
+ */
+function boneLookAlongWorld(bone, direction) {
+  var q = new THREE.Quaternion().setFromUnitVectors(bone.up.normalize(), direction.normalize());
+  if (bone.parent) {
+    let parentQuaternion = bone.parent.getWorldQuaternion(new THREE.Quaternion()).clone();
+    q = q.premultiply(parentQuaternion.invert());
+  }
+  bone.quaternion.copy(q);
+}
+
+/**
+ * Maps the bone names in the avatar rig to the pairs of joint names in the input data
+ *
+ * Each key represents a bone from the avatar rig, and the value is an array of two joint names
+ * representing the joints that the bone connects.
+ * @constant {Object<string, string[]>} boneMappings
+ */
+const boneMappings = {
+  mixamorigLeftArm: ["leftShoulder", "leftElbow"],
+  mixamorigLeftForeArm: ["leftElbow", "leftWrist"],
+  mixamorigRightArm: ["rightShoulder", "rightElbow"],
+  mixamorigRightForeArm: ["rightElbow", "rightWrist"],
+  mixamorig2LeftArm: ["leftShoulder", "leftElbow"],
+  mixamorig2LeftForeArm: ["leftElbow", "leftWrist"],
+  mixamorig2RightArm: ["rightShoulder", "rightElbow"],
+  mixamorig2RightForeArm: ["rightElbow", "rightWrist"],
+};
+
+/**
+ * Sets the pose of the avatar based on the 3D world landmarks data.
+ * @param {THREE.Object3D} avatar - The avatar object to change posture of.
+ * @param {Object<string, number[]>} landmarks - The 3D world landmarks data, mapping name of a joint to [x, y, z] coordinates of the landmark in global space. Joint names must be present in `boneMappings` constant.
+ * @example
+ * setPose(avatar, {"leftShoulder": [0, 0, 0], "leftElbow": [0, 1, 0]}); // sets the pose of the avatar to have the left arm pointing upwards
+ */
+function setPose(avatar, landmarks) {
+  for (const [rigBoneName, jointNames] of Object.entries(boneMappings)) {
+    if (landmarks[jointNames[0]] && landmarks[jointNames[1]]) {
+      let bone = avatar.getObjectByName(rigBoneName);
+      if (!bone) continue;
+
+      let direction = new THREE.Vector3().subVectors(
+        new THREE.Vector3().fromArray(landmarks[jointNames[1]]),
+        new THREE.Vector3().fromArray(landmarks[jointNames[0]])
+      );
+      boneLookAlongWorld(bone, direction);
+    }
+  }
+  avatar.updateMatrixWorld(); // ! needed??
+}
+
+/**
+ * Animates the avatar based on the 3D world landmarks data.
+ * @param {THREE.Object3D} avatar - The avatar object to change posture of.
+ * @param {Object<string, number[]>[]} frames - Array of 3D world landmarks data, each index corresponds to 1 frame/timeStep/pose. Each frame's object maps name of a body joint to its [x, y, z] coordinates in global space. Joint names must be present in `boneMappings` constant.
+ * @param {number} fps - The frame rate of the animation (e.g. 30).
+ * @example
+ * poseAnimation(avatar, [{"leftShoulder": [0, 0, 0], "leftElbow": [0, 1, 0]}, {"leftShoulder": [0, 0, 0], "leftElbow": [0, 0, 1]}], 1); // animates the avatar to point the left arm upwards and then forwards at 1 framePerSecond.
+ */
+function poseAnimation(avatar, frames, fps) {
+  if (frames.length === 0) return;
+  let index = 0;
+
+  let interval = setInterval(
+    function () {
+      if (index < frames.length) {
+        setPose(avatar, frames[index]);
+        index++;
+      } else {
+        // Stop the loop when data ends
+        clearInterval(interval);
+      }
+    },
+    fps ? 1000 / fps : 50
+  );
+}
 
 export default function Avatar({ points }) {
-  let inputAnim = {};
+  let frames = [];
   if (isJsonString(points)) {
-    inputAnim = JSON.parse(points);
+    frames = JSON.parse(points);
   }
 
-  // ! ==== Initialize the avatar ==== ! //
-  // load the avatar
-  const avatarGLTF = useGLTF("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/stacy_lightweight.glb");
-  const avatar = avatarGLTF.scene;
+  // ==== Initialize the avatar ==== //
+  const scene = new THREE.Scene();
+  // var avatar = useFBX("src/assets/avatar/mixamo-bot.fbx");
+  // avatar.scale.set(0.01, 0.01, 0.01); // cm to m
+  var avatar = loadStacy();
 
-  // load & apply skin texture
-  let avatarTexture = new THREE.TextureLoader().load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/stacy.jpg");
-  avatarTexture.flipY = false;
-  const avatarMaterial = new THREE.MeshPhongMaterial({
-    map: avatarTexture,
-    color: 0xffffff,
-  });
-  avatar.traverse((obj3d) => {
-    if (obj3d.isMesh) {
-      obj3d.castShadow = true;
-      obj3d.receiveShadow = true;
-      obj3d.material = avatarMaterial;
-    }
-  });
+  scene.add(avatar);
 
-  // Adjust avatar size & placement
-  avatar.position.y = -3;
-  avatar.scale.set(3, 3, 3);
+  scene.position.y = -2.5;
+  scene.scale.set(2.5, 2.5, 2.5);
 
-  // ! ==== Animate the avatar ==== ! //
-  // bind joints to variables
-  let leftShoulder = avatar.getObjectByName("mixamorigLeftArm"),
-    leftElbow = avatar.getObjectByName("mixamorigLeftForeArm"),
-    leftWrist = avatar.getObjectByName("mixamorigLeftHand"),
-    rightShoulder = avatar.getObjectByName("mixamorigRightArm"),
-    rightElbow = avatar.getObjectByName("mixamorigRightForeArm"),
-    rightWrist = avatar.getObjectByName("mixamorigRightHand"),
-    rightPinkyTip = avatar.getObjectByName("mixamorigRightHandPinky3"),
-    rightRingTip = avatar.getObjectByName("mixamorigRightHandRing3");
+  // ==== Animate the avatar ==== //
+  poseAnimation(scene, frames, 25);
 
-  // detach joints from their parent bones and attach them to the avatar
-  // to make them transform in world space
-  avatar.attach(leftShoulder);
-  avatar.attach(rightShoulder);
-  avatar.attach(leftElbow);
-  avatar.attach(rightElbow);
-
-  // leftShoulder.scale.set(new THREE.Vector3(1, 2, 1).normalize());
-
-  if (inputAnim.name) {
-    const mixer = new THREE.AnimationMixer(avatar);
-    // const clock = new THREE.Clock();
-
-    // Start the animation
-    function update(t) {
-      mixer.update(0.05);
-      requestAnimationFrame(update);
-    }
-
-    // * Bind an animation to the avatar
-    let inputAnimClip = THREE.AnimationClip.parse(inputAnim);
-    let inputAnimJointsNames = inputAnimClip.tracks.map((track) => track.name.split(".")[0]);
-
-    let idleAnimClip = THREE.AnimationClip.findByName(avatarGLTF.animations, "idle").clone();
-    let useableIdleTracks = idleAnimClip.tracks.filter(
-      (track) => !inputAnimJointsNames.includes(track.name.split(".")[0])
-    );
-
-    if (useableIdleTracks.length > 0) {
-      let newTimeSteps = [];
-      let numTimeSteps = useableIdleTracks[0].times.length;
-      for (let i = 1; i <= numTimeSteps; i++) {
-        newTimeSteps.push((inputAnimClip.duration * i) / numTimeSteps);
-      }
-
-      for (let i = 0; i < useableIdleTracks.length; i++) {
-        useableIdleTracks[i].times = newTimeSteps;
-        inputAnimClip.tracks.push(useableIdleTracks[i]);
-      }
-    }
-
-    console.log(inputAnimClip);
-
-    const animAction = mixer.clipAction(inputAnimClip);
-    animAction.play();
-    // console.log(THREE.AnimationClip.toJSON(idleAnim));
-    // idleAnim.tracks.push(pos);
-
-    update();
-  }
   return (
     <div id="canvas-container">
       <Canvas fallback={<div>Sorry no WebGL supported!</div>}>
-        {/* <Environment files="/img/hdri/vaporwave.jpg" background /> */}
-        <ambientLight intensity={0.69} />
-        <primitive object={avatarGLTF.scene} />
-        <axesHelper args={[1]} />
+        <ambientLight intensity={0.5} />
+        <pointLight position={[2, 4, 5]} intensity={25} />
+        <primitive object={scene} />
         <OrbitControls />
+        {/* <Environment files="src/assets/avatar/hdri-pultusk-castle.jpg" background /> */}
+        {/* <axesHelper args={[1]} /> */}
         {/* <Stats /> */}
       </Canvas>
     </div>
   );
+}
+
+/**
+ * https://tympanus.net/codrops/2019/10/14/how-to-create-an-interactive-3d-character-with-three-js/
+ * @returns {THREE.Object3D} - The avatar object.
+ */
+function loadStacy() {
+  let avatar = useGLTF("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/stacy_lightweight.glb").scene;
+  avatar.traverse((o) => {
+    if (o.isMesh) {
+      o.castShadow = true;
+      o.receiveShadow = true;
+
+      let texture = new THREE.TextureLoader().load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/1376484/stacy.jpg");
+      texture.flipY = false;
+
+      o.material = new THREE.MeshPhongMaterial({
+        map: texture,
+        color: 0xffffff,
+        skinning: true,
+      });
+    }
+  });
+  return avatar;
 }
--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,51 +1,52 @@
-import * as THREE from "three";
-
-function setWorldRotation(joint, x = 0, y = 0, z = 0, add = false) {
-  const parent = joint.parent;
-  joint.removeFromParent();
-  if (add) {
-    joint.rotation.x += x;
-    joint.rotation.y += y;
-    joint.rotation.z += z;
-  } else {
-    joint.rotation.set(x, y, z);
-  }
-  parent.attach(joint);
-  return joint;
-}
-
-function setWorldQuaternion(joint, x = 0, y = 0, z = 0, w = 1) {
-  const parent = joint.parent;
-  joint.removeFromParent();
-  joint.quaternion.set(x, y, z, w);
-  parent.attach(joint);
-  return joint;
-}
-
+/**
+ * Check if a string is a valid JSON string
+ * @param {string} str - The string to check
+ * @returns {boolean} - True if the string is a valid JSON string, false otherwise
+ */
 function isJsonString(str) {
   try {
     JSON.parse(str);
   } catch (e) {
-    return false;
+    if (e instanceof SyntaxError) {
+      return false;
+    }
+    throw e;
   }
   return true;
 }
 
-function skeletonInfo(avatar) {
-  let data = {};
-  avatar.traverse((o) => {
-    if (o.isBone) {
-      let worldVector = new THREE.Vector3();
-      o.getWorldPosition(worldVector);
-      data[o.name] = {
-        world: worldVector.toArray(),
-        up: o.up.toArray(),
-        parent: o.parent.name,
-        children: o.children.map((c) => c.name),
-      };
-    }
-  });
-  return data;
+/**
+ * Calculates the angles of a point in 3D space relative to the positive XY, YZ, and ZX planes.
+ * The angles are measured clockwise from the positive quadrant of each plane.
+ *
+ * @param {number} x - The x-coordinate of the point in 3D space.
+ * @param {number} y - The y-coordinate of the point in 3D space.
+ * @param {number} z - The z-coordinate of the point in 3D space.
+ * @returns {Object.<string, number>} An object containing the angles relative to the XY, YZ, and ZX planes in degrees.
+ *  - `xyPlane` - Angle from the XY plane.
+ *  - `yzPlane`: Angle from the YZ plane.
+ *  - `zxPlane`: Angle from the ZX plane.
+ * @example
+ * const angles = calculateAngles(1, 1, 1);
+ * console.log(angles);
+ * // { xyPlane: 35.2, yzPlane: 35.2, zxPlane: 35.2 }
+ */
+function calculateAngles(x, y, z) {
+  // Convert radians to degrees
+  const radToDeg = (/** @type {number} */ radians) => Math.round((radians * 1800) / Math.PI) / 10;
+
+  // Calculate the projection angles relative to each plane
+  const xyPlane = radToDeg(Math.atan2(z, Math.sqrt(x * x + y * y))); // Angle between XY and Z axis
+  const yzPlane = radToDeg(Math.atan2(x, Math.sqrt(y * y + z * z))); // Angle between YZ and X axis
+  const zxPlane = radToDeg(Math.atan2(y, Math.sqrt(x * x + z * z))); // Angle between ZX and Y axis
+
+  return {
+    // todo: adjust according to the quadrant (Clockwise from positive axes)
+    // xyPlane: xyPlane < 0 ? xyPlane + 360 : xyPlane,
+    xyPlane: xyPlane,
+    yzPlane: yzPlane,
+    zxPlane: zxPlane,
+  };
 }
 
-export { setWorldRotation, setWorldQuaternion, isJsonString, skeletonInfo };
+export { isJsonString, calculateAngles };
